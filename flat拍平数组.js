e20ab9d695ccff1276d84dc2211571176238ee66
tomato <h20333@qq.com>
2023-04-14 16:56:35 +0800

    fix:修复bug

--- flat拍平数组.js
+++ flat拍平数组.js
@@ -1,4 +1,4 @@
-const arr = [1, [2, 3, [4, [[5]]]]]
+const arr = [1, [2, 3, [4, [[5]]]]];
 
 console.log(arr.flat(1));
 console.log(arr.flat(2));
@@ -6,15 +6,17 @@ console.log(arr.flat(3));
 console.log(arr.flat(4));
 
 // depth<=0时，返回的数组和原数组维数一样（注意只是维数一样，空位情况见第3点）
-console.log("depth<=0时，返回的数组和原数组维数一样（注意只是维数一样，空位情况见第3点）");
+console.log(
+  "depth<=0时，返回的数组和原数组维数一样（注意只是维数一样，空位情况见第3点）"
+);
 console.log(arr.flat(0));
 
 console.log([].concat(...arr));
 
 // 自己实现一个flat扁平化数组
-function myflat(arr){
-  while(arr.some(item=>Array.isArray(item))){
-      arr = [].concat(...arr)
+function myflat(arr) {
+  while (arr.some((item) => Array.isArray(item))) {
+    arr = [].concat(...arr);
   }
   return arr;
 }
@@ -22,22 +24,22 @@ console.log("我的实现");
 console.log(myflat(arr));
 
 // 重写原型上的方法
-Array.prototype.newflat = function(n=1){
-
+Array.prototype.newflat = function (n = 1) {
   let arr = this;
-  while(n && this.some(item=>Array.isArray(item))){
+  while (n && this.some((item) => Array.isArray(item))) {
     arr = [].concat(...arr);
     n--;
   }
   return arr;
-}
+};
 console.log("重写原型上的方法");
-console.log([1, 2, [3, 4, [5, [6, [7]]]]].newflat(5))
-
+console.log([1, 2, [3, 4, [5, [6, [7]]]]].newflat(5));
 
 // 使用reduce方法拍平数组
-function FlatReduce(arr){
-  return arr.reduce((pre,cur) => {
-    return pre.concat(Array.isArray(cur)?FlatReduce(cur):cur)
-  },[])
+function FlatReduce(arr) {
+  return arr.reduce((pre, cur) => {
+    return pre.concat(Array.isArray(cur) ? FlatReduce(cur) : cur);
+  }, []);
 }
+
+//https://juejin.cn/post/6998879961935249422
--- 数组去重.js
+++ 数组去重.js
@@ -1,22 +1,21 @@
-var array = [1, 2, 1, 1, '1'];
-
+var array = [1, 2, 1, 1, "1"];
 
 // Array.from去重
-function uniques(array){
-  return Array.from(new Set(array));   
+function uniques(array) {
+  return Array.from(new Set(array));
 }
 
 // 简化
-function SampleUniques(array){
-  return [...new Set(array)]
+function SampleUniques(array) {
+  return [...new Set(array)];
 }
 console.log(uniques(array));
 
 // 也可以使用es5中的indexOf方法
-function es5Uniques(array){
-  let res  = array.filter(function(item,index,array){
+function es5Uniques(array) {
+  let res = array.filter(function (item, index, array) {
     return array.indexOf(item) === index;
-  })
+  });
   return res;
 }
 
