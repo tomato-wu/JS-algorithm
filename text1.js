829dab9aeb2b5b1327698f3462c0857ee107f52d
tomato-wu <h20333@qq.com>
2024-03-11 07:31:22 +0800

    fix:修复

--- text1.js
+++ text1.js
@@ -1,6 +1,37 @@
-Function.prototype.myBind = function (context, ...args) {
-    let self = this;
-    return function (...args2) {
-        self.apply(context,args.concat(args2))
-    }
- }
+async function async1() {
+    console.log('async1 start')
+    await async2()
+    console.log('async1 end')
+}
+
+async function async2() {
+    console.log('async2')
+}
+
+console.log('script start')
+
+setTimeout(function () {
+    console.log('setTimeout0')
+}, 0)
+
+setTimeout(function () {
+    console.log('setTimeout2')
+}, 300)
+
+setImmediate(() => console.log('setImmediate'));
+
+process.nextTick(() => console.log('nextTick1'));
+
+async1();
+
+process.nextTick(() => console.log('nextTick2'));
+
+new Promise(function (resolve) {
+    console.log('promise1')
+    resolve();
+    console.log('promise2')
+}).then(function () {
+    console.log('promise3')
+})
+
+console.log('script end')
--- 拍平数组/其他方式.js
+++ 拍平数组/其他方式.js
@@ -1,18 +1,5 @@
 const arr = [1, [2, 3, [4, [[5]]]]];
 
-console.log(arr.flat(1));
-console.log(arr.flat(2));
-console.log(arr.flat(3));
-console.log(arr.flat(4));
-
-// depth<=0时，返回的数组和原数组维数一样（注意只是维数一样，空位情况见第3点）
-console.log(
-  "depth<=0时，返回的数组和原数组维数一样（注意只是维数一样，空位情况见第3点）"
-);
-console.log(arr.flat(0));
-
-console.log([].concat(...arr));
-
 // 自己实现一个flat扁平化数组
 function myflat(arr) {
   while (arr.some((item) => Array.isArray(item))) {
